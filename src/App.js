262dd6c11d217a01ea3e5158b733c8677d4ff8a7
Zhenyuan Gao <31643389+ZhenyuanGao@users.noreply.github.com>
2022-03-29 21:57:47 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+//import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { useState } from "react";
+import { Header } from "./header";
+import { Button } from "antd";
+//import Modal from "./Modal";
+import { MainPage } from "./MainPage";
+//import TextInput from "./textinput";
+import "./email.css";
+function App() {
+  const [visible, setVisible] = useState(true);
+
+  return (
+    <>
+      <div>
+        <Header />
+        <MainPage visible={visible} setvisible={setVisible} />
+      </div>
+    </>
+  );
+}
+
+export default App;
+
+/**
+ * <div>
+      <input
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      ></input>
+      <button onClick={console.log(value)}> click me</button>
+    </div>
+ */
